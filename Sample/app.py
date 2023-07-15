68e89c3b41be4e8120297f5f9c2316912ca14353
vikramv <vk485591@dal.ca>
2023-07-14 21:47:56 -0300

    CORS error resolved

--- Sample/app.py
+++ Sample/app.py
@@ -1,12 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import openai
-import os
+from flask_cors import CORS
+
+# app = Flask(__name__) # Initialize the Flask app
+# CORS(app)
+# CORS(app, methods=['GET', 'POST', 'PUT']) # Allow specific HTTP methods (e.g., GET, POST, PUT)
+# CORS(app, headers=['Content-Type']) # Allow specific headers in the request
+# CORS(app, supports_credentials=True) # Allow cookies to be included in cross-origin requests
+# CORS(app, resources={r"/generate-team-name": {"origins": "*"}})
 
 app = Flask(__name__)
 
+@app.after_request
+def add_headers(response):
+    response.headers.add("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+    return response
+
 # Set OpenAI API key
 # openai.api_key = os.environ["OPENAI_API_KEY"]
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = "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"
+
+
 def generate_gpt3_response(user_text, print_output=False):
     completions = openai.Completion.create(
         engine='text-davinci-003',
@@ -22,11 +37,19 @@ def generate_gpt3_response(user_text, print_output=False):
 
     return completions.choices[0].text
     # return completions
-@app.route("/generate-team-name",methods=["GET"])
+@app.route("/generate-team-name",methods=["POST"])
 def generate_team_name():
-    prompt = "Generate a random team name for a quiz, with a maximum length of 2 words"
+    # user_prompt = request.json['userPrompt']
+    # request_data = request.get_json()
+    # print(request_data)
+    # user_prompt = request_data['userPrompt']
+
+    # print(request.headers)
+    prompt = "Generate a UNIQUE and CREATIVE team name for a quiz game with only one word"#+user_prompt
     response = generate_gpt3_response(prompt)
-    # return jsonify({"team_name": response}),200
-    return response,200
+    response = response.replace('\n', '')
+    print(jsonify(response))
+    return jsonify(response),200
+    # return response,200
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
