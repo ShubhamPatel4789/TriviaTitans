624a5447571af9264ee1edfbcad5bf4f1505d0d4
vikramv <vk485591@dal.ca>
2023-07-25 02:37:09 -0300

    added AddTeamMembersToDB lambda

--- back_end/Lambda/AddTeamMembersToDB.py
+++ back_end/Lambda/AddTeamMembersToDB.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 dynamodb = boto3.client('dynamodb')
 
@@ -6,17 +7,34 @@ def lambda_handler(event, context):
     team_name = event['team_name']
     email = event['email']
 
-    # Update the DynamoDB table with the new "Current Members" attribute
-    response = dynamodb.update_item(
+    # Fetch the DynamoDB item for the team
+    response = dynamodb.get_item(
+        TableName='Teams',
+        Key={'teamName': {'S': team_name}}
+    )
+    
+    # Check if the 'CurrentMembers' key is already present in the item
+    item = response.get('Item', {})
+    current_members = item.get('CurrentMembers', {'L': []})
+    
+    if 'L' in current_members:
+        # 'CurrentMembers' key is already present, so append the email to the list
+        current_members['L'].append({'S': email})
+    else:
+        # 'CurrentMembers' key is not present, create a new one with the email as the first element of the list
+        current_members = {'L': [{'S': email}]}
+    
+    # Update the DynamoDB item with the new 'CurrentMembers' key
+    dynamodb.update_item(
         TableName='Teams',
         Key={'teamName': {'S': team_name}},
-        UpdateExpression='SET #attr = :val',
-        ExpressionAttributeNames={'#attr': 'Current Members'},
-        ExpressionAttributeValues={':val': {'M': {email.replace('@', '_'): {'BOOL': True}}}},
+        UpdateExpression='SET #current_members = :current_members',
+        ExpressionAttributeNames={'#current_members': 'CurrentMembers'},
+        ExpressionAttributeValues={':current_members': current_members}
     )
-    print(f"Added member {email} to 'Current Members' of {team_name}")
 
     return {
         'statusCode': 200,
         'body': 'User added to the "Current Members" attribute.'
     }
+
