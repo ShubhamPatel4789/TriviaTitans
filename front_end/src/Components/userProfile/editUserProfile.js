eb3f1171a732b2f78f4d6e2d5acedb6a7bd7829c
Shubham_Patel <shubhamr1975@gmail.com>
2023-08-06 05:03:26 +0530

    user profile teams

--- front_end/src/Components/userProfile/editUserProfile.js
+++ front_end/src/Components/userProfile/editUserProfile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, Button, TableHead, TableRow, Paper } from '@mui/material';
 
 const EditProfile = () => {
   const userEmail = localStorage.getItem('userEmail');
@@ -9,9 +9,12 @@ const EditProfile = () => {
   const [username, setUsername] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState('');
+  const [teamName, setTeamName] = useState('');
+  const [isInTeam, setIsInTeam] = useState(false);
 
   useEffect(() => {
     fetchUserDetails();
+    fetchUserTeamDetails();
   }, []);
 
   const fetchUserDetails = async () => {
@@ -34,6 +37,79 @@ const EditProfile = () => {
     }
   };
 
+  const fetchUserTeamDetails = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.post(
+        'https://jc51s5am6l.execute-api.us-east-1.amazonaws.com/stage1',
+        {
+          body: JSON.stringify({
+            "user_email": userEmail,
+            "action": "fetch"
+          }),
+        }
+      );
+      console.log('User teams details:', response.data.body);
+      const teamsData = JSON.parse(response.data.body);
+      console.log('Teams data:', teamsData);
+      const userTeams = teamsData.filter(teamName => teamName.CurrentMembers.includes(userEmail));
+      console.log('User teams:', userTeams);
+      setTeamName(userTeams);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+      setIsLoading(false);
+    }
+  };
+
+  const handleLeaveTeam = async () => {
+    setIsLoading(true);
+    try {
+      // Fetch the current team details
+      const response = await axios.post(
+        'https://jc51s5am6l.execute-api.us-east-1.amazonaws.com/stage1',
+        {
+          body: JSON.stringify({
+            "user_email": userEmail,
+            "action": "fetch"
+          }),
+        }
+      );
+      const teamsData = JSON.parse(response.data.body);
+      const team = teamsData[0];
+  
+      // Remove the current user from the CurrentMembers array
+      const updatedMembers = team.CurrentMembers.filter(member => member !== userEmail);
+  
+      // Update the team details with the new CurrentMembers array
+      const updatedTeam = {
+        ...team,
+        CurrentMembers: updatedMembers,
+      };
+      console.log('Updated team:', updatedTeam)
+  
+      // Send the updated team details to the Lambda function
+      const updateResponse = await axios.post(
+        'https://jc51s5am6l.execute-api.us-east-1.amazonaws.com/stage1',
+        {
+          body: JSON.stringify({
+            "user_email": userEmail,
+            "team_name": teamName,
+            "teamName": teamName,
+            "team_data": updatedTeam,
+            "action": "update"
+          }),
+        }
+      );
+      console.log('Leave team response:', updateResponse.data);
+      fetchUserTeamDetails();
+    } catch (error) {
+      console.error('Error leaving team:', error);
+      setIsLoading(false);
+    }
+  };
+
+  
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -78,30 +154,48 @@ const EditProfile = () => {
           </TableRow>
         </TableBody>
       </Table>
-      <h3>Update Personal Information</h3>
-      <form onSubmit={handleFormSubmit}>
-        <label>First Name:</label>
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <label>Last Name:</label>
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <label>Username:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <button type="submit">Save</button>
-      </form>
-    </div>
-  );
+      {teamName.length > 0 && (
+      <div>
+        <h3>Team Information</h3>
+        <ul>
+          {teamName.map(team => (
+            <li key={team.teamName}>
+              <p>Team Name: {team.teamName}</p>
+              {team.currentMembers && (
+                <p>Current Members: {team.currentMembers.join(', ')}</p>
+              )}
+              <Button onClick={handleLeaveTeam} variant="outlined" color="secondary">
+                Leave Team
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )}
+          <h3>Update Personal Information</h3>
+          <form onSubmit={handleFormSubmit}>
+            <label>First Name:</label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <label>Last Name:</label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <label>Username:</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <button type="submit">Save</button>
+          </form>
+        </div>
+      );
 };
 
-export default EditProfile;
+      export default EditProfile;
